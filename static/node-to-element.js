a9e17028830dba0b9143a6b82e0c9b74b11f5158
Justin Massey <justindmassey@gmail.com>
2025-10-10 14:42:05 +0200

    changed rendering logic

--- static/node-to-element.js
+++ static/node-to-element.js
@@ -1,30 +1,15 @@
-import { ol, li } from "./lib/elements.js"
-import renderers from "./renderers.js"
+import { div, ol, li } from "./lib/elements.js";
+import renderers from "./renderers.js";
 
-function NodeToItems(node) {
-    let m = node.name.value.match(/^(:\S+)\s*(.*)/)
-    if(m && renderers[m[1]]) {
-        return [li(renderers[m[1]].render(node, m[2]))]
+export default function NodeToItems(node) {
+  let m = node.name.value.match(/^(:\S+)\s*(.*)/);
+  if (m && renderers[m[1]]) {
+    return renderers[m[1]].render(node, m[2]);
+  } else {
+    let children = ol();
+    for (let child of node.children.children) {
+      children.appendChild(li(NodeToItems(child.node)));
     }
-    if(node.children.children.length) {
-        let n = li(node.name.value)
-        let c = ol()
-        for(let child of node.children.children) {
-            for(let item of NodeToItems(child.node)) {
-                c.appendChild(item)
-            }
-        }
-        return [n, c]
-    } else {
-        return [li(node.name.value)]
-    }
-}
-
-export default function nodeToElement(node) {
-    
-    let list = ol()
-    for(let item of NodeToItems(node)) {
-        list.appendChild(item)
-    }
-    return list
+    return div(div(node.name.value), children);
+  }
 }
