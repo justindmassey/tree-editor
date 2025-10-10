2258e6cfec09e05d05ec12dbb82aaffa706d3eb2
Justin Massey <justindmassey@gmail.com>
2025-10-10 15:10:18 +0200

    renamed function

--- static/node-to-element.js
+++ static/node-to-element.js
@@ -1,14 +1,14 @@
 import { div, ol, li } from "./lib/elements.js";
 import renderers from "./renderers.js";
 
-export default function NodeToItems(node) {
+export default function NodeToElement(node) {
   let m = node.name.value.match(/^(:\S+)\s*(.*)/);
   if (m && renderers[m[1]]) {
     return renderers[m[1]].render(node, m[2]);
   } else {
     let children = ol();
     for (let child of node.children.children) {
-      children.appendChild(li(NodeToItems(child.node)));
+      children.appendChild(li(NodeToElement(child.node)));
     }
     return div(div(node.name.value), children);
   }
