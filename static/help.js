52d6b71b411639086db9c50d9523d0d90d100142
Justin Massey <justindmassey@gmail.com>
2025-11-16 10:50:45 +0100

    formatting

--- static/help.js
+++ static/help.js
@@ -15,10 +15,13 @@ class Help {
   constructor() {
     this.elem = div(
       h1("Help"),
+
       h2("Global Commands"),
       makeReference(globalCommands),
+
       h2("Node Commands"),
       makeReference(nodeCommands),
+
       h2("Widgets"),
       p(
         div("Widgets begin with ", code("-WIDGET ARGUMENT"), "."),
@@ -40,6 +43,7 @@ class Help {
         div("The output is updated in real time.")
       ),
       makeReference(widgets),
+
       h2("Type System"),
       div("Nodes with a name of the form ", code("::TYPE")),
       div("are type definitions."),
@@ -51,6 +55,7 @@ class Help {
       div('The colons ("', code(":"), '") can be escaped with a backslash.'),
       div("Type definitions can inherit from other type definitions."),
       div("Types are updated in real time."),
+
       h2("XML Export"),
       div("Nodes with children become tags."),
       div("Attribute nodes become attributes."),
