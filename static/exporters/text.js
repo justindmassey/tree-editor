e195602ae4e2f699835f9e0b377e0cad9fa45e2a
Justin Massey <justindmassey@gmail.com>
2025-11-25 13:55:09 +0100

    changed default indentation to 4

--- static/exporters/text.js
+++ static/exporters/text.js
@@ -1,7 +1,7 @@
 export default function exportToText(node, indent = 0) {
   let text = " ".repeat(indent) + node.name.value + "\n";
   for (let child of node.children.children) {
-    text += exportToText(child.node, indent + 2);
+    text += exportToText(child.node, indent + 4);
   }
   return text;
 }
