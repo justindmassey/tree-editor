50cbb31919225b358f01b6b7cea1d384014a2950
Justin Massey <justindmassey@gmail.com>
2026-04-06 14:30:41 +0200

    changed widget order

--- static/widgets.js
+++ static/widgets.js
@@ -91,6 +91,46 @@ export default {
       }
     },
   },
+  "-tgl": {
+    description: div(
+      div("toggle"),
+      div("allows you to toggle the visibility of children"),
+      div(code("$expanded"), ": if the children are visible"),
+    ),
+    create(arg) {
+      let expanded = true;
+      if (!("$expanded" in this.attributes)) {
+        this.setAttribute("$expanded", true);
+      } else {
+        expanded = this._attributes.$expanded == "true";
+      }
+      let children = this.childrenWidget;
+      if (!expanded) {
+        children.classList.add("hidden");
+      }
+      let icon;
+      if (expanded) {
+        icon = "▼";
+      } else {
+        icon = "▶";
+      }
+      let toggleButton = span(icon)
+        .c("tgl-icon")
+        .e("click", () => {
+          if (expanded) {
+            this.setAttribute("$expanded", false);
+          } else {
+            this.setAttribute("$expanded", true);
+          }
+          history.add();
+        });
+      if (arg) {
+        return div(div(toggleButton, " ", arg), children.c("indented"));
+      } else {
+        return div(div(toggleButton), children);
+      }
+    },
+  },
   "-hdr": {
     description: div(
       div("large header with the children below"),
@@ -286,7 +326,7 @@ export default {
       div(code("argument"), ": card header"),
       div("Children become the card body."),
       div("Attributes become a table at the bottom of the card."),
-      div(code("$"), "-attributes are ignored.")
+      div(code("$"), "-attributes are ignored."),
     ),
     create(arg) {
       let attrTable = table().c("crd-attr-table");
@@ -450,46 +490,6 @@ export default {
         return div(arg, " ", pager, div(page).c("indented"));
       } else {
         return div(pager, div(page));
-      }
-    },
-  },
-  "-tgl": {
-    description: div(
-      div("toggle"),
-      div("allows you to toggle the visibility of children"),
-      div(code("$expanded"), ": if the children are visible"),
-    ),
-    create(arg) {
-      let expanded = true;
-      if (!("$expanded" in this.attributes)) {
-        this.setAttribute("$expanded", true);
-      } else {
-        expanded = this._attributes.$expanded == "true";
-      }
-      let children = this.childrenWidget;
-      if (!expanded) {
-        children.classList.add("hidden");
-      }
-      let icon;
-      if (expanded) {
-        icon = "▼";
-      } else {
-        icon = "▶";
-      }
-      let toggleButton = span(icon)
-        .c("tgl-icon")
-        .e("click", () => {
-          if (expanded) {
-            this.setAttribute("$expanded", false);
-          } else {
-            this.setAttribute("$expanded", true);
-          }
-          history.add();
-        });
-      if (arg) {
-        return div(div(toggleButton, " ", arg), children.c("indented"));
-      } else {
-        return div(div(toggleButton), children);
       }
     },
   },
