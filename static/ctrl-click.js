0bc496f6a9d4f94284f0e67d7ff8ca9f67d086ed
Justin Massey <justindmassey@gmail.com>
2026-04-10 18:35:43 +0200

    moved ctrlClick to Element.prototype

--- static/ctrl-click.js
+++
@@ -1,8 +0,0 @@
-export default function ctrlClick(node, event) {
-  if (event.ctrlKey) {
-    event.stopPropagation();
-    event.stopImmediatePropagation();
-    event.preventDefault();
-    node.focus();
-  }
-}
